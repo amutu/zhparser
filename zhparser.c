5fe59489a24ed13b4efdf7b69eddf9acdd5f86b6
Jov <amutu@amutu.com>
2025-01-24 15:42:57 +0800

    do not report error(NOTICE) to client when user not config custom dict
    make the message level to LOG, only write to server log file

--- zhparser.c
+++ zhparser.c
@@ -215,9 +215,9 @@ static void init(){
 	snprintf(dict_path, MAXPGPATH, "%s/base/zhprs_dict_%s.txt",
 			DataDir, get_database_name(MyDatabaseId));
 	if(scws_add_dict(scws, dict_path, load_dict_mem_mode | SCWS_XDICT_TXT) != 0 ){
-		ereport(NOTICE,
+		ereport(LOG,
 			    (errcode(ERRCODE_INTERNAL_ERROR),
-			     errmsg("zhparser add dict : \"%s\" failed!",dict_path
+			     errmsg("zhparser add dict : \"%s\" failed! May not config custom dict, omit this",dict_path
 				 )));
 	}
 
